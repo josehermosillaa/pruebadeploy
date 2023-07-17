571064e244767be1bc9b6780d64895fdacb244c6
josehermosilla <jose.hermosillaa@usach.cl>
2023-07-17 11:27:39 -0400

    otro admin

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser2 --username admin2 --password mysecretadminpassword
+python manage.py createsuperuser --username admin --password mysecretadminpassword
