df2c1fd69fe5c4548ef27b10ab68296a2d8c25cb
josehermosilla <jose.hermosillaa@usach.cl>
2023-07-17 11:06:22 -0400

    buildsh test

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 set -o errexit
 
 #poetry install
-# pip install -r requirements.txt
+pip install -r requirements.txt
 python manage.py collectstatic --no-input
+python manage.py makemigrations
 python manage.py migrate
