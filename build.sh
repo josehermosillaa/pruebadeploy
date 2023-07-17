f01864e237fcdbeff65b9aa9a8a7154ffcd94835
josehermosilla <jose.hermosillaa@usach.cl>
2023-07-17 11:41:37 -0400

    superuser noinput

--- build.sh
+++ build.sh
@@ -7,4 +7,14 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser --username admin --password mysecretadminpassword
+
+DJANGO_SUPERUSER_EMAIL=jose@email.com
+export DJANGO_SUPERUSER_EMAIL
+
+DJANGO_SUPERUSER_USERNAME=josehermosillaa
+export DJANGO_SUPERUSER_USERNAME
+
+DJANGO_SUPERUSER_PASSWORD=Jha25101991#
+export DJANGO_SUPERUSER_PASSWORD
+
+python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL --password $DJANGO_SUPERUSER_PASSWORD
