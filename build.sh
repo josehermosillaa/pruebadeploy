dfed11558fa5c953a32093c06e1db4cbb649c79b
josehermosilla <jose.hermosillaa@usach.cl>
2023-07-17 11:48:53 -0400

    other prueba

--- build.sh
+++ build.sh
@@ -8,13 +8,4 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
 
-DJANGO_SUPERUSER_EMAIL=jose@email.com
-export DJANGO_SUPERUSER_EMAIL
-
-DJANGO_SUPERUSER_USERNAME=josehermosillaa
-export DJANGO_SUPERUSER_USERNAME
-
-DJANGO_SUPERUSER_PASSWORD=Jha25101991
-export DJANGO_SUPERUSER_PASSWORD
-
-python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL --password $DJANGO_SUPERUSER_PASSWORD
+python manage.py createsuperuser --username myuser --email myemail@example.com --password mypassword
